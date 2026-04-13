71e3872e4c67baadeb932f9e7130278a1294f121
Altair <alt_abenov@kbtu.kz>
2026-04-13 23:38:22 +0500

    init: create Angular project and move to frontend folder

---
+++ FinanceTracker/frontend/src/app/features/budgets/budget-list/budget-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-list',
+  imports: [],
+  templateUrl: './budget-list.html',
+  styleUrl: './budget-list.css',
+})
+export class BudgetList {
+
+}
